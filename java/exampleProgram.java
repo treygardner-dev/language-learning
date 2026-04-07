99aad970d63e4e57851e662fcca3aade34184331
treygardner-dev <treygardner.dev@protonmail.com>
2026-04-06 22:19:36 -0600

    format fixes, outline created

---
+++ java/exampleProgram.java
@@ -0,0 +1,23 @@
+/*
+
+ * ExampleProgram
+ * 
+ * April 7th, 2026
+ * 
+ * No Copyright 
+ */
+
+//packages & imports
+
+//1 public class
+public class ExampleProgram {
+    
+    //main - program entry point
+    public static void main(String[] args) {
+        //main logic
+        System.out.println("Hello, World!");
+
+    }
+}
+
+//private classes
