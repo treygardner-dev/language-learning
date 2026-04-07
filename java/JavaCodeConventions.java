73d0601135e98c9caaa9a02d7cccc03283759431
treygardner-dev <treygardner.dev@protonmail.com>
2026-04-07 08:16:05 -0600

    starting example program

--- java/JavaCodeConventions.java
+++ java/JavaCodeConventions.java
@@ -6,21 +6,24 @@
 
 //All Java Standards can be found at: https://www.oracle.com/java/technologies/javase/codeconventions-introduction.html
 
+public class JavaCodeConventions {
+    public static void main(String[] args) {
+
 //All Data Types
 
 //Primative Data Types
-boolean bool = False; //true or false (1 bit)
+boolean isTrue = false; //true or false (1 bit)
 char character = 'a'; //single unicode character (16 bits / 2 bytes) '\u0000'
 byte num_1 = 0; //-128 to 127 (8 bits / 1 byte)
 short num_2 = 0; //-32768 to 32767 (16 bits / 2 bytes)
 int num_3 = 0; //-2.1B to 2.1B (32 bits / 4 bytes)
 long num_4 = 0L; //double int, whole numbers (64 bits / 8 bytes)
 float num_5 = 0.0f; //fractional number (32 bits / 4 bytes)
-double num_6 0.0d; //fractional numbers (64 bits / 8 bytes)
+double num_6 = 0.0d; //fractional numbers (64 bits / 8 bytes)
 
 //Non-Primative Data Types (Reference)
 String word = "Hello, World!"; //a sequence of characters (dynamic size)
-int num_array = [1, 2, 3, 4]; //a collection of primative data, all the same type (dynamic size)
+int num_array[] = {1, 2, 3, 4}; //a collection of primative data, all the same type (dynamic size)
 //Classes; //Object blueprints: user created (dynamic size)
 //Interfaces; //abstract type, collection of methods: user created (dynamic size)
 //Enums; //a collection of constants: user created (dynamic size)
@@ -59,3 +62,5 @@ int num_array = [1, 2, 3, 4]; //a collection of primative data, all the same typ
  */
 
 
+    }
+}
--- java/exampleProgram.java
+++ java/exampleProgram.java
@@ -7,17 +7,42 @@
  * No Copyright 
  */
 
-//packages & imports
+/* Libraries */
 
-//1 public class
+/** One Public Class 
+ * 
+ * create and manage a list of clients
+*/
 public class ExampleProgram {
     
-    //main - program entry point
+    /* main entry point */
     public static void main(String[] args) {
-        //main logic
+
         System.out.println("Hello, World!");
 
     }
 }
 
-//private classes
+/* Private Classes */
+class Client {
+    private String name;
+    private String address;
+    private float quote;
+    private float paid;
+    private boolean paidInFull;
+
+    public Client (String name, String address, float quote, float paid) {
+        this.name = name;
+        this.address = address;
+        this.quote = quote;
+        this.paid = paid;
+        this.paidInFull = ((quote - paid) == 0) ? true : false;
+    }
+
+    public String printClient() {
+        return "\nClient Name: " + this.name + "\nClient Address: " 
+        + this.address + "\n";
+    }
+
+
+}
